e033f6b2ddb02bf55f81d8f29f270d250c637092
Dmytro Yaremyshyn <dmytroy@dxspark.com>
2024-09-11 17:44:58 +0100

    Created project

---
+++ Spotify/Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Dmytro Yaremyshyn on 11/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
